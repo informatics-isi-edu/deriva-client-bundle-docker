cf09e3abc0c123a8cf4b2a9483e81f2d66206995
mikedarcy <mikedarcy@users.noreply.github.com>
2019-06-11 23:03:09 -0700

    Update for deriva-catalog-manage dependency refactor.

--- win/buildenv/build-deriva-client-dev/build-dev.ps1
+++ win/buildenv/build-deriva-client-dev/build-dev.ps1
@@ -4,7 +4,7 @@ pip install setuptools_scm 2>&1 | %{"$_"}
 pip install bdbag[boto,globus] 2>&1 | %{"$_"}
 pip install git+https://github.com/informatics-isi-edu/deriva-py.git 2>&1 | %{"$_"}
 pip install git+https://github.com/informatics-isi-edu/deriva-qt.git 2>&1 | %{"$_"}
-pip install git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git 2>&1 | %{"$_"}
+pip install git+https://github.com/informatics-isi-edu/deriva-catalog-manage.git#egg=deriva-catalog-manage[csv] 2>&1 | %{"$_"}
 git clone https://github.com/informatics-isi-edu/deriva-client-bundle 2>&1 | %{"$_"}
 cd deriva-client-bundle
 python setup.py bdist_msi 2>&1 | %{"$_"}
--- win/buildenv/build-deriva-client-release/build-release.ps1
+++ win/buildenv/build-deriva-client-release/build-release.ps1
@@ -1,6 +1,6 @@
 pip install urllib3==1.24.3 2>&1 | %{"$_"}
 pip install setuptools_scm 2>&1 | %{"$_"}
-pip install deriva-client 2>&1 | %{"$_"}
+pip install deriva-client[all] 2>&1 | %{"$_"}
 git clone https://github.com/informatics-isi-edu/deriva-client-bundle 2>&1 | %{"$_"}
 cd deriva-client-bundle
 python setup.py bdist_msi 2>&1 | %{"$_"}
